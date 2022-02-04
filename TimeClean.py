857bdf61d4a052edc94e13603f4a597fb11ade44
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-02-04 13:55:13 +0100

    Timeclean finished

--- TimeClean.py
+++ TimeClean.py
@@ -4,57 +4,61 @@ df = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet da
                  dtype={"Initial start": "str", "Start": "str", "Stop": "str"})
 
 df['Datetime'] = df['Date'] + " " + df['Time']
-df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y %m %d %H:%M:%S')
+df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M:%S')
 print(df.head())
-#df['Datetime'].replace({pd.NaT: 'new_sheep'}, inplace=True)
-print(df['Datetime'][20].day-df['Datetime'][21].day)
 
 month_end = [30, 31]
-temp = 1  # telle hvilket datasett det er
-count = 0
-check = []
+temp = 1
 i = 1
-while i < (len(df)-1):
-    if pd.isnull(df.loc[i + 1, 'Datetime']):
+while i < (len(df)-2):
+    if pd.isnull(df.at[i + 1, 'Datetime']):
         temp += 1
         i += 2  # start the next check on the next data set
         continue
 
-    year_diff = df.loc[i+1, 'Datetime'].year - df.loc[i, 'Datetime'].year
-    month_diff = df.loc[i+1, 'Datetime'].month - df.loc[i, 'Datetime'].month
-    day_diff = (df['Datetime'][i+1] - df['Datetime'][i]).components[0]
-    #hour_diff = (df['Datetime'][i+1]-df['Datetime'][i]).components[1]
-    #minute_diff = (df['Datetime'][i+1]-df['Datetime'][i]).components[2]
-
-    # sjekke om dato/tidspunkt er helt like?
+    year_diff = df.at[i+1, 'Datetime'].year - df.at[i, 'Datetime'].year
+    month_diff = df.at[i+1, 'Datetime'].month - df.at[i, 'Datetime'].month
+    day_diff = df.at[i+1, 'Datetime'].day - df.at[i, 'Datetime'].day
 
     if year_diff != 0:
-        df.loc[i + 1, 'Datetime'] = df.loc[i + 1, 'Datetime'].replace(year=df.loc[i, 'Datetime'].year)
+        df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(year=df.at[i, 'Datetime'].year)
         continue  # check again that it works before moving on
 
     if abs(month_diff) > 1:
-        df.loc[i + 1, 'Datetime'] = df.loc[i + 1, 'Datetime'].replace(month=df.loc[i, 'Datetime'].month)
+        df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(month=df.at[i, 'Datetime'].month)
         continue
 
     if month_diff == 1 and df['Datetime'][i].day not in month_end:
-        df.loc[i + 1, 'Datetime'] = df.loc[i + 1, 'Datetime'].replace(month=df.loc[i, 'Datetime'].month)
+        if df['Datetime'][i+2].month == df['Datetime'][i].month:
+            df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(month=df.at[i, 'Datetime'].month)
+        else:
+            i += 1
         continue
 
     if month_diff == -1:
-        df.loc[i + 1, 'Datetime'] = df.loc[i + 1, 'Datetime'].replace(month=df.loc[i, 'Datetime'].month)
+        df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(month=df.at[i, 'Datetime'].month)
         continue
 
-    if abs(day_diff) > 1:
-        df.loc[i + 1, 'Datetime'] = df.loc[i + 1, 'Datetime'].replace(day=df.loc[i, 'Datetime'].day)
+    if abs(day_diff) > 1 and df['Datetime'][i].day not in month_end:
+        df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(day=df.at[i, 'Datetime'].day)
         continue
 
-    if day_diff == 1 and df['Datetime'][i].time().hour != 23:
-        df.loc[i + 1, 'Datetime'] = df.loc[i + 1, 'Datetime'].replace(day=df.loc[i, 'Datetime'].day)
+    if day_diff == 1 and df['Datetime'][i].time().hour < 22:
+        if df['Datetime'][i + 2].day == df['Datetime'][i].day:
+            df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(day=df.at[i, 'Datetime'].day)
+        else:
+            i += 1
         continue
 
     if day_diff == -1:
-        df.loc[i + 1, 'Datetime'] = df.loc[i + 1, 'Datetime'].replace(day=df.loc[i, 'Datetime'].day)
+        df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(day=df.at[i, 'Datetime'].day)
         continue
+
+    time_diff = pd.Timedelta(abs(df['Datetime'][i + 1] - df['Datetime'][i])).seconds / (60 * 60)
+    if time_diff > 2:
+        if pd.Timedelta(abs(df['Datetime'][i + 2] - df['Datetime'][i])).seconds / (60 * 60) < 2:
+            mid_diff = pd.Timedelta(abs(df.at[i+2, 'Datetime'] - df.at[i, 'Datetime'])).seconds / 2
+            df.at[i + 1, 'Datetime'] = df.at[i, 'Datetime'] + pd.Timedelta(hours=mid_diff/(60*60))
 
     if (i % 10000) == 0:
         # check progress against number of iterations
@@ -62,11 +66,5 @@ while i < (len(df)-1):
 
     i += 1
 
-print(check)
-print(count)
-
-# plotte datoene for å se om det stemmer?
-
-#df = df.drop(columns=['Date', 'Time'])  # deleting the uncleaned columns that we no longer need
-#print(df.head())
-#df.to_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll dato.csv', index=False, sep=';')
+df = df.drop(columns=['Date', 'Time'])  # deleting the (uncleaned) columns that we no longer need
+#df.to_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll.csv', index=False, sep=';')
