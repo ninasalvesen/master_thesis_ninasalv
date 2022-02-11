7eb302df23ccbd07df67ed0b90ed88d02aecc8db
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-02-11 14:11:57 +0100

    finished timeclean both sets

--- TimeClean.py
+++ TimeClean.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 df1 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll uten timeclean.csv', delimiter=';',
                  dtype={"Initial start": "str", "Start": "str", "Stop": "str"})
@@ -10,14 +9,20 @@ df1['Datetime'] = df1['Date'] + " " + df1['Time']
 df1['Datetime'] = pd.to_datetime(df1['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
 df2['Datetime'] = pd.to_datetime(df2['Datetime'], format='%d/%m/%Y %H:%M')
+print(df1.head())
+print(df2.head())
 
 
-def timeclean(df, delta):
+def TimeClean(df, delta):
     month_end = [30, 31]
     count = 0
     temp = 1     # data set number, skips the first row so must be initialized at 1
     i = 1        # have to check the first and two last lines for faulty points manually
     while i < (len(df)-2):
+        if (i % 10000) == 0:
+            # check progress against number of iterations
+            print("Reached number: ", i)
+
         if pd.isnull(df.at[i + 1, 'Datetime']):
             temp += 1
             i += 2  # start the next check on the next data set
@@ -29,20 +34,17 @@ def timeclean(df, delta):
 
         if year_diff != 0:
             count += 1
-            print('year', i)
             df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(year=df.at[i, 'Datetime'].year)
             continue  # check again that it works before moving on
 
         if abs(month_diff) > 1:
             count += 1
-            print('month', i)
             df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(month=df.at[i, 'Datetime'].month)
             continue
 
         if month_diff == 1 and df['Datetime'][i].day not in month_end:
             if df['Datetime'][i+2].month == df['Datetime'][i].month:
                 count += 1
-                print('month2', i)
                 df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(month=df.at[i, 'Datetime'].month)
             else:
                 i += 1
@@ -50,20 +52,20 @@ def timeclean(df, delta):
 
         if month_diff == -1:
             count += 1
-            print('month3', i)
             df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(month=df.at[i, 'Datetime'].month)
             continue
 
         if abs(day_diff) > 1 and df['Datetime'][i].day not in month_end:
-            count += 1
-            print('day', i)
-            df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(day=df.at[i, 'Datetime'].day)
+            if df.at[i+2, 'Datetime'].day == df.at[i, 'Datetime'].day:
+                count += 1
+                df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(day=df.at[i, 'Datetime'].day)
+            else:
+                i += 1
             continue
 
         if day_diff == 1 and df['Datetime'][i].time().hour < 22:
             if df['Datetime'][i + 2].day == df['Datetime'][i].day:
                 count += 1
-                print('day2', i)
                 df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(day=df.at[i, 'Datetime'].day)
             else:
                 i += 1
@@ -71,32 +73,28 @@ def timeclean(df, delta):
 
         if day_diff == -1:
             count += 1
-            print('day3', i)
             df.at[i + 1, 'Datetime'] = df.at[i + 1, 'Datetime'].replace(day=df.at[i, 'Datetime'].day)
             continue
 
-        time_diff = pd.Timedelta(np.abs(df['Datetime'][i + 1] - df['Datetime'][i])).seconds / (60 * 60)
-        if time_diff > delta:     # delta enables the user to choose the time difference limit in hours
-            if pd.isnull(df.at[i + 2, 'Datetime']) == False and pd.Timedelta(np.abs(df['Datetime'][i + 2] - df['Datetime'][i])).seconds / (60 * 60) < delta:
+        # delta enables the user to choose the time difference limit in hours
+        if ((df.at[i + 1, 'Datetime'] - df.at[i, 'Datetime']).total_seconds() / (60*60)) > delta:
+            if pd.isnull(df.at[i + 2, 'Datetime']) == False \
+                    and ((df.at[i + 2, 'Datetime'] - df.at[i, 'Datetime']).total_seconds() / (60*60)) < delta:
                 count += 1
-                mid_diff = pd.Timedelta(np.abs(df.at[i+2, 'Datetime'] - df.at[i, 'Datetime'])).seconds / 2
+                mid_diff = (df.at[i + 2, 'Datetime'] - df.at[i, 'Datetime']).total_seconds() / 2
                 df.at[i + 1, 'Datetime'] = df.at[i, 'Datetime'] + pd.Timedelta(hours=mid_diff/(60*60))
-                print('time', i)
-
-        if (i % 10000) == 0:
-            # check progress against number of iterations
-            print("Reached number: ", i)
 
         i += 1
     return df, count
 
 
-#df1, count = timeclean(df1, 2)
-#df1 = df1.drop(columns=['Date', 'Time'])  # deleting the (uncleaned) columns that we no longer need
-#df1.to_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll.csv', index=False, sep=';')
+df1, count1 = TimeClean(df1, 2)
+print(count1)
+df1 = df1.drop(columns=['Date', 'Time'])  # deleting the (uncleaned) columns that we no longer need
+df1.to_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll1.csv', index=False, sep=';')
 
-df2, count = timeclean(df2, 4)
-#df2 = df2.drop(columns=['Date.Time'])
-#df2.to_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen.csv', index=False, sep=';')
+df2, count2 = TimeClean(df2, 4)
+print(count2)
+df2 = df2.drop(columns=['Date.Time'])
+df2.to_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen1.csv', index=False, sep=';')
 
-print(count)
