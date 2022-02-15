b22e94552782452f42f65619c3cc34355ceea47e
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-02-15 13:46:47 +0100

    finished pointclean Fosen

--- PointClean.py
+++ PointClean.py
@@ -1,10 +1,33 @@
 import pandas as pd
+import math
 
-df2 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen.csv', delimiter=';')
+df2 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen uten pointclean.csv',
+                  delimiter=';')
 df2['Datetime'] = pd.to_datetime(df2['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
 
-def remove(df):
+def zeros(df):  # remover all zeros at beginning and end of sets
+    i = 2
+    while i < (len(df)):
+        if (i % 10000) == 0:
+            # check progress against number of iterations
+            print("Reached number: ", i)
+        temp = i
+        n = 0
+        while df.at[temp, 'Lat'] == 0:
+            temp += 1
+            n += 1
+        if pd.isnull(df.at[temp, 'Datetime']) and n > 0:  # null på slutten av et sett
+            df = df.drop(df.index[i:temp])
+            df.reset_index(inplace=True, drop=True)
+        if pd.isnull(df.at[i-1, 'Datetime']) and n > 0:  # null på starten av et sett
+            df = df.drop(df.index[i:temp])
+            df.reset_index(inplace=True, drop=True)
+        i += 1
+    return df
+
+
+def remove(df):  # removes first and last 5 points in each set
     temp = 0     # data set number
     i = 0        # have to check the first and two last lines for faulty points manually
     while i < (len(df)):
@@ -23,45 +46,57 @@ def remove(df):
     return df
 
 
-def zeros(df):
+def impute(df):  # imputes average points where there are zeros
     i = 0
-    check = 0
-    while i < (len(df)-2):
+    while i < (len(df)):
         if (i % 10000) == 0:
             # check progress against number of iterations
             print("Reached number: ", i)
         if pd.isnull(df.at[i, 'Datetime']):
             i += 1  # start the next check on the next data set
             continue
-        if df.at[i, 'Lat'] == 0 and df.at[i + 1, 'Lat'] != 0:
-            df.at[i, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[i + 1, 'Lat']) / 2
-            df.at[i, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[i + 1, 'Lon']) / 2
-            continue
-        if df.at[i, 'Lat'] == 0 and df.at[i + 1, 'Lat'] == 0:
-            if df.at[i + 2, 'Lat'] != 0:
-                df.at[i, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[i + 2, 'Lat']) / 2
-                df.at[i, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[i + 2, 'Lon']) / 2
-                continue
-        if df.at[i, 'Lat'] == 0 and df.at[i + 1, 'Lat'] == 0:
-            if df.at[i + 2, 'Lat'] == 0 and df.at[i + 3, 'Lat'] != 0:
-                df.at[i, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[i + 3, 'Lat']) / 2
-                df.at[i, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[i + 3, 'Lon']) / 2
-                continue
-
-        if df.at[i, 'Lat'] == 0 and df.at[i + 1, 'Lat'] == 0:
-            if df.at[i + 2, 'Lat'] == 0 and df.at[i + 3, 'Lat'] == 0:
-                check += 1
-                i += 4
-                continue
-        # heller prøve å se på å finne neste punkt som ikke er null med en ny while løkke, og iterere nedover
-        # helt til alle null-punkter er oppfyllt
+        temp = i
+        n = 0
+        while df.at[temp, 'Lat'] == 0:
+            temp += 1
+            n += 1
+        if n % 2 == 0:
+            if n >= 4:
+                n = int(n / 2)
+                df.at[i + n, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[temp, 'Lat']) / 2
+                df.at[i + n, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[temp, 'Lon']) / 2
+                while n > 1:
+                    n -= 1
+                    df.at[i + n, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[i + n + 1, 'Lat']) / 2
+                    df.at[i + n, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[i + n + 1, 'Lon']) / 2
+                    df.at[temp - n, 'Lat'] = (df.at[temp - n - 1, 'Lat'] + df.at[temp, 'Lat']) / 2
+                    df.at[temp - n, 'Lon'] = (df.at[temp - n - 1, 'Lon'] + df.at[temp, 'Lon']) / 2
+                df.at[i, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[i + 1, 'Lat']) / 2
+                df.at[i, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[i + 1, 'Lon']) / 2
+            if n == 2:
+                df.at[i, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[temp + 1, 'Lat']) / 2
+                df.at[i, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[temp + 1, 'Lon']) / 2
+                df.at[i + 1, 'Lat'] = (df.at[i, 'Lat'] + df.at[temp + 1, 'Lat']) / 2
+                df.at[i + 1, 'Lon'] = (df.at[i, 'Lon'] + df.at[temp + 1, 'Lon']) / 2
+        if n % 2 != 0:
+            if n >= 3:
+                n = math.floor(n / 2)
+                df.at[i + n, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[temp, 'Lat']) / 2
+                df.at[i + n, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[temp, 'Lon']) / 2
+                while n > 0:
+                    n -= 1
+                    df.at[i + n, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[i + n + 1, 'Lat']) / 2
+                    df.at[i + n, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[i + n + 1, 'Lon']) / 2
+                    df.at[temp - n - 1, 'Lat'] = (df.at[temp - n - 2, 'Lat'] + df.at[temp, 'Lat']) / 2
+                    df.at[temp - n - 1, 'Lon'] = (df.at[temp - n - 2, 'Lon'] + df.at[temp, 'Lon']) / 2
+            else:
+                df.at[i, 'Lat'] = (df.at[i - 1, 'Lat'] + df.at[i + 1, 'Lat']) / 2
+                df.at[i, 'Lon'] = (df.at[i - 1, 'Lon'] + df.at[i + 1, 'Lon']) / 2
         i += 1
-    return df, check
+    return df
 
-print(df2[2386:2393])
-df2, test = zeros(df2)
-print(df2[2386:2393])
-print('sjekk:', test)
-#df2 = remove(df2)
-#print(df2.head())
-#df2.to_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/test.csv', index=False, sep=';')
+
+df2 = zeros(df2)
+df2 = remove(df2)
+df2 = impute(df2)
+df2.to_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen.csv', index=False, sep=';')
