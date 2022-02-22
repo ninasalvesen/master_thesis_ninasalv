beb7e19af22c644fa954a5278ce3dbd9c60160f6
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-02-22 12:24:23 +0100

    added map plot for fosen

--- MapPlotFosen.py
+++ MapPlotFosen.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-df = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll V3 med Haversine.csv', delimiter=';',
-                 dtype={"Initial start": "str", "Start": "str", "Stop": "str"})
-df['Datetime'] = pd.to_datetime(df['Datetime'], format='%d/%m/%Y %H:%M')
+df = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen V4 med Haversine.csv',
+                 delimiter=';')
+df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
 print(df.head())
 
@@ -34,23 +33,19 @@ while i < len(df):
         break
     i += 1
 
-
 print("Time frame for the time spent on pastures: ", time)
 extreme_values = [min(long), max(long), min(lat), max(lat)]
 print("extreme values: ", extreme_values)
 
 
-# Choose which home range to look at by changing home_range and fig1
-home_range = 'koksvik'
-figure_koksvik = [8.1230, 8.4114, 62.8333, 62.9393]
-figure_torjul = [8.0576, 8.3460, 62.9087, 63.0232]
-
-figure = plt.imread(f"/Users/ninasalvesen/Documents/Sauedata/Bilder/figure_{home_range}.png")
-fig1 = figure_koksvik
+figure1 = plt.imread(f"/Users/ninasalvesen/Documents/Sauedata/Bilder/figure_fosen_close.png")
+fig1 = [10.1856, 10.4740, 63.6959, 63.8074]
+figure2 = plt.imread(f"/Users/ninasalvesen/Documents/Sauedata/Bilder/figure_fosen_far.png")
+fig2 = [10.1720, 10.4604, 63.7823, 63.8934]
 
 fig, ax = plt.subplots()
 ax.scatter(long, lat, zorder=1, alpha=0.6, c="b", s=1)
-ax.imshow(figure, zorder=0, extent=fig1, aspect="auto")
+ax.imshow(figure1, zorder=0, extent=fig1, aspect="auto")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid("True")
