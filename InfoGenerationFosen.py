ef65c67dd5def6a6832f7e19f149e50869602885
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-03-07 23:01:54 +0100

    info generation standardized and fixed

--- InfoGenerationFosen.py
+++ InfoGenerationFosen.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen uten timeclean.csv', delimiter=';')
+df = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen V4 med Haversine.csv', delimiter=';')
 print(df.head())
-df['Datetime'] = pd.to_datetime(df['Datetime'], format='%d/%m/%Y %H:%M')
+df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
-df2 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/NIBIO_Telespor_lamsøye.csv', delimiter=';',
-                  encoding='latin-1')
+df2 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/NIBIO_Telespor_lamsøye.csv', delimiter=';', encoding='latin-1')
 
 df2['uniq.log'] = df2['Telespor_id'].astype(str) + '_' + df2['yr'].astype(str)
 df2['uniq.log_mor'] = df2['M_Telespor_id'].astype(str) + '_' + df2['yr'].astype(str)
 
-df3 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Informasjon datasett Fosen.csv', delimiter=';')
+df3 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Informasjon datasett Fosen.csv', delimiter=';', encoding='latin-1')
 df3['i, slutt'] = np.nan
-
+print(df2.head())
+print(df3.head())
 
 sett = 0
 i = 0
@@ -25,8 +25,6 @@ while i < len(df):
         df3['exists1'] = df3['uniq.log'].isin(df2['uniq.log'])
         df3['exists2'] = df3['uniq.log'].isin(df2['uniq.log_mor'])
 
-        df3["exists1"] = df3['uniq.log'].isin(df2['uniq.log'])
-        df3["exists2"] = df3['uniq.log'].isin(df2['uniq.log_mor'])
         if df3.at[df3[df3['uniq.log'] == df3.at[sett, 'uniq.log']].index.values[0], 'exists1']:
             index = df2[df2['uniq.log'] == df3.at[sett, 'uniq.log']].index.values[0]
             df3.at[sett, 'rase'] = df2.at[index, 'Rase']
@@ -34,16 +32,19 @@ while i < len(df):
             index = df2[df2['uniq.log_mor'] == df3.at[sett, 'uniq.log']].index.values[0]
             df3.at[sett, 'rase'] = df2.at[index, 'Rase']
 
-
         if sett > 0:
             df3.at[sett - 1, 'Slutt'] = df.at[i - 1, 'Datetime']
-            df3.at[sett - 1, 'i, slutt'] = i-1
+            df3.at[sett - 1, 'i, slutt'] = i - 1
+            if sett > 2:
+                df3.at[sett - 1, 'Size'] = df3.at[sett - 1, 'i, slutt'] - df3.at[sett - 2, 'i, slutt'] - 1
         sett += 1
-
     i += 1
+    if i == len(df) - 1:
+        df3.at[sett - 1, 'Slutt'] = df.at[i, 'Datetime']
+        df3.at[sett - 1, 'i, slutt'] = i
+        df3.at[sett - 1, 'Size'] = df3.at[sett - 1, 'i, slutt'] - df3.at[sett - 2, 'i, slutt'] - 1
     if (i % 10000) == 0:
         # check progress against number of iterations
         print("Reached number: ", i)
 
-
-df3.to_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Informasjon datasett Fosen.csv', index=False, sep=';')
+df3.to_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Informasjon datasett Fosen sjekk.csv', index=False, sep=';')
---
+++ InfoGenerationTingvoll.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll V3 med Haversine.csv',
+                 delimiter=';', dtype={"Initial start": "str", "Start": "str", "Stop": "str"})
+print(df.head())
+df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M:%S')
+
+df2 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Informasjon datasett Tingvoll.csv', delimiter=';')
+print(df2.head())
+
+sett = 0
+i = 0
+while i < len(df):
+    if df.at[i, 'Data set'] == 'new_sheep':
+        df2.at[sett, 'Sett'] = sett + 1
+        df2.at[sett, 'Start'] = df.at[i + 1, 'Datetime']
+
+        if sett > 0:
+            df2.at[sett - 1, 'Slutt'] = df.at[i - 1, 'Datetime']
+            df2.at[sett - 1, 'i, slutt'] = i - 1
+            if sett > 1:
+                df2.at[sett - 1, 'Size'] = df2.at[sett - 1, 'i, slutt'] - df2.at[sett - 2, 'i, slutt'] - 1
+        sett += 1
+    i += 1
+    if i == len(df) - 1:
+        df2.at[sett - 1, 'Slutt'] = df.at[i, 'Datetime']
+        df2.at[sett - 1, 'i, slutt'] = i
+        df2.at[sett - 1, 'Size'] = df2.at[sett - 1, 'i, slutt'] - df2.at[sett - 2, 'i, slutt'] - 1
+        df2.at[0, 'Size'] = df2.at[0, 'i, slutt']
+    if (i % 10000) == 0:
+        # check progress against number of iterations
+        print("Reached number: ", i)
+
+df2.to_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Informasjon datasett Tingvoll.csv', index=False, sep=';')
