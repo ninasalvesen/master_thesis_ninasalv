fafe0ad897c6a138f4525a298852581105af972e
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-04-04 17:23:47 +0200

    included fosen analysis in kmeans

--- Kmeans.py
+++ Kmeans.py
@@ -14,11 +14,22 @@ df1 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet d
 
 df1['Datetime'] = pd.to_datetime(df1['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
-df1.dropna(subset=['Datetime'], inplace=True)
-df1.reset_index(inplace=True, drop=True)
-df1['XY'] = np.sqrt(df1['X']**2 + df1['Y']**2)
-df1 = df1.drop(columns=['Datetime', 'Lat', 'X', 'Y', 'Lon', 'Data set', 'uniq.log', 'Farm', 'minutes'])
+df2 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen V7 sinetime uten 2018.csv',
+                 delimiter=';', low_memory=False)
 
+df2['Datetime'] = pd.to_datetime(df2['Datetime'], format='%d/%m/%Y %H:%M')
+
+#df1.dropna(subset=['Datetime'], inplace=True)
+#df1.reset_index(inplace=True, drop=True)
+#df1['XY'] = np.sqrt(df1['X']**2 + df1['Y']**2)
+#df1 = df1.drop(columns=['Datetime', 'Lat', 'X', 'Y', 'Lon', 'Data set', 'uniq.log', 'Farm', 'minutes'])
+
+df2.dropna(subset=['Datetime'], inplace=True)
+df2.reset_index(inplace=True, drop=True)
+df2 = df2.drop(columns=['Datetime', 'Lat', 'Lon', 'Data set', 'uniq.log', 'minutes'])
+
+#print(df2)
+#print(df2.describe())
 #print(df1)
 #print(df1.describe())
 
@@ -43,13 +54,19 @@ def Normalize(df, columns):  # normalizes the columns listed in the variable col
     return df
 
 
+df2 = Standardize(df2, ['Haversine'])
+df2 = Normalize(df2, ['Haversine'])  # skal jeg ha med begge her?
+print(df2)
+#print(df2.describe())
+
+"""
 df1 = Standardize(df1, ['Haversine', 'XY'])
 df1 = Normalize(df1, ['Haversine', 'XY'])
 print(df1)
 #print(df1.describe())
 
 
-
+# TINGVOLL:
 # Elbow method of finding number of clusters for optimal kmeans
 sse = {}
 for k in range(1, 20):
@@ -65,7 +82,7 @@ plt.grid(True)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.tight_layout()
-#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/elbow_kmeans_tingvoll.png", dpi=500)
+#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/kmeans/tingvoll_elbow_kmeans.png", dpi=500)
 plt.show()
 # optimal k=6
 
@@ -104,7 +121,7 @@ plt.ylabel('Cumulative explained variance', fontsize=25, labelpad=15)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.tight_layout()
-#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/PCA_feature_variance_tingvoll.png", dpi=500)
+#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/kmeans/tingvoll_PCA_feature_variance.png", dpi=500)
 plt.show()
 # bør holde ca. 0.8 av variansen, her bør dimensjonen bli 2
 
@@ -128,7 +145,7 @@ plt.grid(True)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.tight_layout()
-#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/elbow_kmeans_PCA_tingvoll.png", dpi=500)
+#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/kmeans/tingvoll_elbow_kmeans_PCA.png", dpi=500)
 plt.show()
 # optimal k=4
 
@@ -150,8 +167,55 @@ plt.ylabel('PC2', fontsize=25, labelpad=15)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.tight_layout()
-#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/kmeans_PCA_tingvoll.png", dpi=500)
+#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/kmeans/tingvoll_kmeans_PCA.png", dpi=500)
 plt.show()
 
 #df1_reduced['cluster'] = clusters
 #print(df1_reduced)
+
+
+
+# FOSEN:
+# Elbow method of finding number of clusters for optimal kmeans
+sse = {}
+for k in range(1, 20):
+    print(k)
+    kmeans = KMeans(n_clusters=k, max_iter=1000).fit(df2)
+    df2['clusters'] = kmeans.labels_  # .labels_ er det samme som .predict når man putter inn samme datasett i predict
+    sse[k] = kmeans.inertia_  # Inertia: Sum of distances of samples to their closest cluster center
+plt.figure(figsize=(16, 8))
+plt.plot(list(sse.keys()), list(sse.values()))  # gjør dette til ditt eget så det ikke kan plagieres?
+plt.title('Elbow method for finding number of clusters in Kmeans++', fontsize=35, pad=15)
+plt.xlabel('Number of clusters', fontsize=25, labelpad=15)
+plt.ylabel('SSE', fontsize=25, labelpad=15)
+plt.grid(True)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.tight_layout()
+#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/after cut 4.0/kmeans/fosen_u2018_elbow_kmeans.png", dpi=500)
+plt.show()
+# optimal k=4
+"""
+kmeans = KMeans(n_clusters=4)
+# forklaring på hver iterasjon og toleranse + center shift: verbose=2 i Kmeans
+kmeans.fit(df2)
+centroids = kmeans.cluster_centers_
+print(centroids)
+clusters = kmeans.labels_  # samme som kmeans.predict(df1)
+df2['cluster'] = clusters
+print(df2)
+print('inertia:', kmeans.inertia_)
+print('iterations:', kmeans.n_iter_)
+print('n features:', kmeans.n_features_in_)
+print('features:', kmeans.feature_names_in_)
+# print('Silhouette score:', metrics.silhouette_score(df1, clusters))
+# kjører veldig lenge O(n**2), ikke bruk på så mye data
+
+fig2 = px.scatter_3d(df2, x='sin_time', y='cos_time', z='Haversine', color='cluster', opacity=1)
+fig2.update_layout(title='3D cluster rep. Fosen')
+fig2.update_traces(marker=dict(size=5, line=dict(width=1, color='white')), selector=dict(mode='markers'))
+fig2.show()
+
+
+
+
