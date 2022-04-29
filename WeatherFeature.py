eca0fc4624a164a43eb3dec0d4637f6e5c090908
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-04-29 11:22:04 +0200

    finished temp feature

--- WeatherFeature.py
+++ WeatherFeature.py
@@ -9,23 +9,17 @@ df2 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet 
                   delimiter=';', low_memory=False)
 df2['Datetime'] = pd.to_datetime(df2['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
-df3 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/weather Tingvoll 2012.csv', delimiter=';')
+df3 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/weather Tingvoll.csv', delimiter=';')
 df3['Datetime'] = pd.to_datetime(df3['Tid(norsk normaltid)'], format='%d.%m.%Y %H:%M')
 
 df4 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/weather Fosen.csv', delimiter=';')
 df4['Datetime'] = pd.to_datetime(df4['Tid(norsk normaltid)'], format='%d.%m.%Y %H:%M')
 
-#print(df1)
-#print(df2)
+
 df3.drop(columns=['Navn', 'Stasjon', 'Tid(norsk normaltid)'], inplace=True)
 df4.drop(columns=['Navn', 'Stasjon', 'Nedbør (12 t)', 'Tid(norsk normaltid)'], inplace=True)
-df1['Temp'] = None
-df2['Temp'] = None
-
-print(df1)
-print(df3)
-#print(df2.head())
-#print(df4)
+#df1['Temp'] = None
+#df2['Temp'] = None
 
 
 def initTime(df_temp):
@@ -48,17 +42,48 @@ def GetTemp(df, df_temp):
             i += 1  # start the next check on the next data set in the second point
             continue
 
-        index = df_temp[(df_temp['Date'] == df.at[i, 'Datetime'].date()) &
-                        (df_temp['Hour'] == df.at[i, 'Datetime'].hour)].index.values[0]
-        df.at[i, 'Temp'] = df_temp.at[index, 'Lufttemperatur']
+        try:
+            index = df_temp[(df_temp['Date'] == df.at[i, 'Datetime'].date()) &
+                            (df_temp['Hour'] == df.at[i, 'Datetime'].hour)].index.values[0]
+            df.at[i, 'Temp'] = df_temp.at[index, 'Lufttemperatur']
+        except:
+            print(df.at[i, 'Datetime'], i)
 
         i += 1
 
     return df
 
 
-df3 = initTime(df3)
-df4 = initTime(df4)
-print(df3)
-test = GetTemp(df1, df3)
-print(test)
+def ImputeTemp(df):
+    df['Temp'] = df['Temp'].str.replace(',', '.').astype(float)
+    i = 0
+    while i < len(df):
+        if (i % 10000) == 0:
+            # check progress against number of iterations
+            print("Reached number: ", i)
+
+        if pd.isnull(df.at[i, 'Datetime']):
+            i += 1  # start the next check on the next data set in the second point
+            continue
+
+        try:
+            if pd.isnull(df.at[i, 'Temp']):
+                df.at[i, 'Temp'] = (df.at[i - 1, 'Temp'] + df.at[i + 1, 'Temp']) / 2
+        except:
+            print(i, df.at[i, 'Datetime'])
+            print(df.at[i - 1, 'Temp'])
+            print(df.at[i + 1, 'Temp'])
+
+        i += 1
+    return df
+
+
+#df3 = initTime(df3)
+#temp1 = GetTemp(df1, df3)
+#temp1 = ImputeTemp(temp1)
+#temp1.to_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet data Tingvoll V9 med temp.csv', index=False, sep=';')
+
+#df4 = initTime(df4)
+#temp2 = GetTemp(df2, df4)
+#temp2 = ImputeTemp(temp2)
+#temp2.to_csv('/Users/ninasalvesen/Documents/Sauedata/Fosen_Telespor/Samlet data Fosen V10 med temp.csv', index=False, sep=';')
