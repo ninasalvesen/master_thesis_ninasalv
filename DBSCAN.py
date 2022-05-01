9a2d00fa714d330592193e81d6149ec08353fea3
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-05-01 19:33:15 +0200

    Update DBSCAN.py

--- DBSCAN.py
+++ DBSCAN.py
@@ -11,8 +11,6 @@ df1 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Tingvoll data/Samlet d
 
 df1['Datetime'] = pd.to_datetime(df1['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
-print(df1)
-
 
 # removing empty rows at the beginning of each set
 df1.dropna(subset=['Datetime'], inplace=True)
@@ -21,26 +19,24 @@ df1.reset_index(inplace=True, drop=True)
 # chose which features to include
 #df1['XY'] = np.sqrt(df1['X']**2 + df1['Y']**2)
 df1 = df1.drop(columns=['Datetime', 'Lat', 'X', 'Y', 'Lon', 'Data set', 'uniq.log', 'Farm', 'minutes',
-                        'age', 'n_lambs', 'race', 'besetning', 'Temp'])
+                        'race', 'besetning'])
 print(df1)
 
 def dbscan(df, epsilon, min):
-    print('hei')
     db = DBSCAN(eps=epsilon, min_samples=min).fit(df1)
-    print('test')
     clusters = db.labels_
     df['clusters'] = clusters
 
     print('n features:', db.n_features_in_)
     print('features:', db.feature_names_in_)
 
-    no_clusters = len(np.unique(labels))
-    no_noise = np.sum(np.array(labels) == -1, axis=0)
+    no_clusters = len(np.unique(clusters))
+    no_noise = np.sum(np.array(clusters) == -1, axis=0)
 
     print('Estimated no. of clusters: %d' % no_clusters)
     print('Estimated no. of noise points: %d' % no_noise)
 
-    fig = px.scatter_3d(df, x='sin_time', y='cos_time', z='Haversine', color='cluster', opacity=1)
+    fig = px.scatter_3d(df, x='sin_time', y='cos_time', z='Haversine', color='clusters', opacity=1)
     fig.update_traces(marker=dict(size=5), selector=dict(mode='markers'))
     # fig = px.scatter_3d(df, x='sin_time', y='cos_time', z='Haversine', size='XY', color='cluster', opacity=1)
     fig.update_layout(title='3D cluster rep. Tingvoll DBSCAN')
@@ -49,7 +45,8 @@ def dbscan(df, epsilon, min):
 
 
 # Tingvoll
-df1 = Standardize(df1, ['Haversine'])
-df1 = Normalize(df1, ['Haversine'], -1, 1)
+df1 = Standardize(df1, ['Haversine', 'age', 'n_lambs', 'Temp'])
+df1 = Normalize(df1, ['Haversine', 'age', 'n_lambs', 'Temp'], -1, 1)
 #print(df1.describe())
-dbscan(df1, 1, 15)
+print(df1)
+dbscan(df1, 0.2, 12)
