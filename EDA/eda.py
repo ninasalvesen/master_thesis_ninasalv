fa015534db021f824ee95f6baf3520ac3508c177
ninasalvesen <78792277+ninasalvesen@users.noreply.github.com>
2022-05-15 14:01:54 +0200

    Update eda.py

--- EDA/eda.py
+++ EDA/eda.py
@@ -5,32 +5,35 @@ import seaborn as sns
 sns.set_style('darkgrid')
 
 
-df1 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Datasett_ferdig/Endelig/Total begge.csv',
+df1 = pd.read_csv('/Users/ninasalvesen/Documents/Sauedata/Datasett_ferdig/Endelig/Total age ok.csv',
                   sep=';', dtype={"Initial start": "str", "Start": "str", "Stop": "str"})
 df1['Datetime'] = pd.to_datetime(df1['Datetime'], format='%Y-%m-%d %H:%M:%S')
 
 df1.dropna(subset=['Datetime'], inplace=True)
 df1.reset_index(inplace=True, drop=True)
 
-df1 = df1.drop(columns=['Datetime', 'Lat', 'Lon', 'Data set', 'uniq.log',
-                        'race', 'besetning'])
+df1 = df1.drop(columns=['Datetime', 'Lat', 'Lon', 'Data set', 'uniq.log', 'race', 'besetning'])
 
-
+df1.rename(columns={'Haversine':'Velocity'}, inplace=True)
 print(df1)
 print(df1.columns)
-print(df1.describe())
 
-#print(df2)
-
-
-#plt.figure()
-#corr = df1.corr()
-#sns.heatmap(corr)
-#plt.show()
-
-plt.figure(figsize=(16, 16))
-pd.plotting.scatter_matrix(df1)
+plt.figure(figsize=(10, 5))
+corr = df1.corr()
+sns.heatmap(corr, annot=True, linewidths=0.5, vmin=-1, vmax=1)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.title('Heatmap of feature correlation', fontsize=25, pad=15)
 plt.tight_layout()
+#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/01 Total/heatmap.png", dpi=500)
+plt.show()
+
+
+plt.figure()
+sns.pairplot(df1, diag_kind='hist')
+corr = df1.corr()
+plt.tight_layout()
+#plt.savefig("/Users/ninasalvesen/Documents/Sauedata/Bilder/Master/01 Total/pairplot.png", dpi=500)
 plt.show()
 
 
